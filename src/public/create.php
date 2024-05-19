9def7c5306d5d25df555e3db7d0a8dd6e9afc46b
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-05-19 17:59:23 +0900

    feat: 書籍登録

---
+++ src/public/create.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>書籍登録</title>
+</head>
+<body>
+  <h2>書籍を登録</h2>
+  <form action="store.php" method="get">
+    <label for="" name="title">タイトル</label>
+    <input type="text" name="title" placeholder="タイトル">
+    <label for="" name="inmpressions">感想</label>
+    <input type="text" name="impressions" placeholder="感想"><br>
+    <button type="submit">登録</button>
+  </form>
+  
+</body>
+</html>
